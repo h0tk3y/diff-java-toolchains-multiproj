b69ed24f72c15505ffb0d0b15f29b599e8effed7
Sergey Igushkin <hotkeytlt@gmail.com>
2023-09-14 16:07:02 +0400

    Improve the IDE workaround
    With this option, the IDE will analyze every
    project with `recompile-with-java-downgrade`
    plugin against the downgraded Java version.

--- buildSrc/src/main/kotlin/recompile-with-java-downgrade.gradle.kts
+++ buildSrc/src/main/kotlin/recompile-with-java-downgrade.gradle.kts
@@ -11,44 +11,53 @@ abstract class JavaDowngradeExtension {
 
 val javaDowngradeExtension = extensions.create<JavaDowngradeExtension>("javaDowngrade")
 
-java {
-    val main by sourceSets.existing
-    val javaDowngrade by sourceSets.creating {
-        tasks.withType<JavaCompile>().named(compileJavaTaskName).configure {
-            setSource(main.flatMap { tasks.withType<JavaCompile>().named(it.compileJavaTaskName) }.map { it.source })
-            javaCompiler.set(javaToolchains.compilerFor {
-                languageVersion.set(javaDowngradeExtension.toVersion.map(JavaLanguageVersion::of))
-            })
+if (System.getProperty("idea.sync.active") == "true") {
+    afterEvaluate {
+        java {
+            toolchain.languageVersion.set(javaDowngradeExtension.toVersion.map(JavaLanguageVersion::of))
         }
     }
-    registerFeature("javaDowngrade") {
-        usingSourceSet(javaDowngrade)
-        // Make the feature variants expose the default capability, so they can be used as a replacement for the "normal" variants
-        capability("${project.group}", project.name, "${project.version}")
-    }
+} else run {
+    java {
+        val main by sourceSets.existing
+        val javaDowngrade by sourceSets.creating {
+            tasks.withType<JavaCompile>().named(compileJavaTaskName).configure {
+                setSource(main.flatMap { tasks.withType<JavaCompile>().named(it.compileJavaTaskName) }
+                    .map { it.source })
+                javaCompiler.set(javaToolchains.compilerFor {
+                    languageVersion.set(javaDowngradeExtension.toVersion.map(JavaLanguageVersion::of))
+                })
+            }
+        }
 
-    plugins.withId("org.jetbrains.kotlin.jvm") {
-        with(project.the<KotlinJvmProjectExtension>()) {
-            val mainKotlin = target.compilations.named("main")
-            target.compilations.named(javaDowngrade.name) {
-                compileTaskProvider {
-                    this as KotlinCompile
-                    setSource(mainKotlin.flatMap { it.compileTaskProvider }.map { (it as KotlinCompile).sources })
-                    kotlinOptions {
-                        this.jvmTarget = javaDowngradeExtension.toVersion.get().toString()
+        plugins.withId("org.jetbrains.kotlin.jvm") {
+            with(project.the<KotlinJvmProjectExtension>()) {
+                val mainKotlin = target.compilations.named("main")
+                target.compilations.named(javaDowngrade.name) {
+                    compileTaskProvider {
+                        this as KotlinCompile
+                        setSource(mainKotlin.flatMap { it.compileTaskProvider }.map { (it as KotlinCompile).sources })
+                        kotlinOptions {
+                            this.jvmTarget = javaDowngradeExtension.toVersion.get().toString()
+                        }
                     }
                 }
             }
         }
-    }
 
-    with(javaDowngrade) {
-        for (configurationName in listOf(apiElementsConfigurationName, runtimeElementsConfigurationName)) {
-            configurations.named(configurationName) {
-                attributes.attributeProvider(
-                    TargetJvmVersion.TARGET_JVM_VERSION_ATTRIBUTE,
-                    javaDowngradeExtension.toVersion
-                )
+        registerFeature("javaDowngrade") {
+            usingSourceSet(javaDowngrade)
+            // Make the feature variants expose the default capability, so they can be used as a replacement for the "normal" variants
+            capability("${project.group}", project.name, "${project.version}")
+        }
+        with(javaDowngrade) {
+            for (configurationName in listOf(apiElementsConfigurationName, runtimeElementsConfigurationName)) {
+                configurations.named(configurationName) {
+                    attributes.attributeProvider(
+                        TargetJvmVersion.TARGET_JVM_VERSION_ATTRIBUTE,
+                        javaDowngradeExtension.toVersion
+                    )
+                }
             }
         }
     }
--- project-x/build.gradle.kts
+++ project-x/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
  `java-library`
- `consume-java-downgrade`
  kotlin("jvm")
 }
 
